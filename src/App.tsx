eb86b3905c0417dc642e2883e17e6259706f1667
GeorgiaTsoukala <georgiats1999@gmail.com>
2026-03-18 21:58:34 +0100

    emoji cursor

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ function App() {
   return (
     <div>
       <h1>Georgia's Portfolio</h1>
+
+      <button style={{ cursor: 'pointer' }}>
+        Hover me 👀
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { CursorifyProvider } from '@cursorify/react'
+import { EmojiCursor } from '@cursorify/cursors'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CursorifyProvider
+      enabled={true}
+      cursor={<EmojiCursor />}
+      delay={2}
+      opacity={1}
+      defaultCursorVisible={false}
+      breakpoint={997}
+    >
+      <App />
+    </CursorifyProvider>
   </StrictMode>,
 )
