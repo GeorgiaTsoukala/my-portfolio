8ace4e2e9e20ee2df9b27d3593bc8e168e13425f
GeorgiaTsoukala <georgiats1999@gmail.com>
2026-04-05 16:26:32 +0200

    sticker addition

--- src/content/aboutStickers.ts
+++ src/content/aboutStickers.ts
@@ -4,6 +4,10 @@ import greeceSticker from '../assets/stickers/greeceSticker.png'
 import emailSticker from '../assets/stickers/emailSticker.png'
 import linkedinSticker from '../assets/stickers/linkedinSticker.png'
 import bikeSticker from '../assets/stickers/bikeSticker.png'
+import githubSticker from '../assets/stickers/githubSticker.png'
+import dtuComputeSticker from '../assets/stickers/dtuComputeSticker.png'
+import internshipSticker from '../assets/stickers/internshipSticker.png'
+import masterSticker from '../assets/stickers/masterSticker.png'
 
 export type AboutSticker = {
   id: string
@@ -23,14 +27,14 @@ export const aboutStickers: AboutSticker[] = [
     text: 'Love basketball. Olympiakooooooos!!!!',
     position: 'top-[15%] left-[2%]',
     size: 'w-[clamp(4.5rem,8vw,6rem)]',
-    rotation: 'rotate-[-8deg]',
+    rotation: 'rotate-[-9deg]',
   },
   {
     id: 'phone',
     image: phoneSticker,
     alt: 'Phone sticker',
     text: 'Always happy to chat! +45 918118100000000000',
-    position: 'bottom-[5%] left-[10%]',
+    position: 'bottom-[5%] left-[12%]',
     size: 'w-[clamp(4.5rem,8vw,7rem)]',
     rotation: 'rotate-[10deg]',
   },
@@ -38,7 +42,7 @@ export const aboutStickers: AboutSticker[] = [
     id: 'greece',
     image: greeceSticker,
     alt: 'Greece sticker',
-    text: "Yep I'm greek.",
+    text: "Yep I'm greek. (It's pronounced 'Yeeros' not 'Jairos')",
     position: 'bottom-[8%] left-[82%]',
     size: 'w-[clamp(4.5rem,8vw,9rem)]',
     rotation: 'rotate-[0deg]',
@@ -59,7 +63,7 @@ export const aboutStickers: AboutSticker[] = [
     text: "Find me ....",
     position: 'bottom-[75%] left-[90%]',
     size: 'w-[clamp(4.5rem,8vw,5rem)]',
-    rotation: 'rotate-[-5deg]',
+    rotation: 'rotate-[15deg]',
   },
   {
     id: 'bike',
@@ -68,7 +72,43 @@ export const aboutStickers: AboutSticker[] = [
     text: "Based in Copenhagen.",
     position: 'bottom-[70%] left-[40%]',
     size: 'w-[clamp(4.5rem,8vw,25rem)]',
-    rotation: 'rotate-[5deg]',
+    rotation: 'rotate-[6deg]',
+  },
+  {
+    id: 'github',
+    image: githubSticker,
+    alt: 'Github sticker',
+    text: "Wanna take a closer look at my projects?",
+    position: 'bottom-[22%] left-[35%]',
+    size: 'w-[clamp(4.5rem,8vw,5.5rem)]',
+    rotation: 'rotate-[-6deg]',
+  },
+  {
+    id: 'dtuCompute',
+    image: dtuComputeSticker,
+    alt: 'DtuCompute sticker',
+    text: "I was a TA.",
+    position: 'bottom-[65%] left-[23%]',
+    size: 'w-[clamp(4.5rem,8vw,5rem)]',
+    rotation: 'rotate-[3deg]',
+  },
+  {
+    id: 'internship',
+    image: internshipSticker,
+    alt: 'Internship sticker',
+    text: "Yep I'm greek. (It's pronounced 'Yeeros' not 'Jairos')",
+    position: 'bottom-[2%] left-[55%]',
+    size: 'w-[clamp(4.5rem,8vw,9rem)]',
+    rotation: 'rotate-[4deg]',
+  },
+  {
+    id: 'master',
+    image: masterSticker,
+    alt: 'Master sticker',
+    text: "master at DTU",
+    position: 'bottom-[75%] left-[63%]',
+    size: 'w-[clamp(4.5rem,8vw,7rem)]',
+    rotation: 'rotate-[-5deg]',
   },
 ]
 
